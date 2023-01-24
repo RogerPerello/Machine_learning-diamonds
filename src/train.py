13eb2d2256f0d8f54b6e87ccc291fad21a6c5245
Roger Perelló <109803180+RogerPerello@users.noreply.github.com>
2023-01-24 14:37:40 +0100

    end

--- src/train.py
+++ src/train.py
@@ -10,7 +10,6 @@ df_diamonds = pd.read_csv(os.path.abspath(path_df))
 X = df_diamonds.drop(columns='price (dollars)')
 y = df_diamonds['price (dollars)']
 
-
 regression = LinearRegression()
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, random_state=43)
 regression.fit(X_train, y_train)
