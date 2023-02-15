4ddb17a520b7ded2feeef732ecf3ece54c616c74
Roger Perelló <109803180+RogerPerello@users.noreply.github.com>
2023-02-15 17:48:56 +0100

    update

--- .streamlit/launcher.py
+++ .streamlit/launcher.py
@@ -1,11 +1,6 @@
 import os
 from subprocess import run
 
+os.chdir('..')
 
-start = False
-while start == False:
-    try:
-        run('streamlit run .streamlit\\files\\main.py')
-        start = True
-    except Exception:
-        os.chdir('..')
+run('streamlit run .streamlit\\files\\main.py')
