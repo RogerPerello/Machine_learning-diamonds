ba3f1aed6fed514d972b8df5a26f1a0328027bd7
Roger Perelló <109803180+RogerPerello@users.noreply.github.com>
2023-02-15 23:15:55 +0100

    update

--- .streamlit/files/prediction_from_characteristics_page.py
+++ .streamlit/files/prediction_from_characteristics_page.py
@@ -15,7 +15,7 @@ def predict_from_characteristics():
         st.write('Try to be precise when assigning key values.')
         input_weight = st.number_input('Weight (carat)', min_value=0.01, max_value=10.0, step=0.01)
         input_depth = st.number_input('Depth (millimeters)', min_value=0.01, max_value=50.0, step=0.01)
-        st.write('----- Fill only the diameter, if your diamond is rounded, or the lenght/width if it is squared. -----')
+        st.write('----- Fill only the diameter, if your diamond is rounded, or the lenght/width if it is squared -----')
         st.write('- For squared diamonds:')
         input_lenght = st.number_input('Lenght (millimeters)', min_value=0.0, max_value=80.0, step=0.01)
         input_width = st.number_input('Width (millimeters)', min_value=0.0, max_value=80.0, step=0.01)
@@ -23,38 +23,44 @@ def predict_from_characteristics():
         input_diameter = st.number_input('Diameter (millimeters)', min_value=0.0, max_value=60.0, step=0.01)
         st.subheader('Secondary values')
         st.write('Try to be precise, but if you do not know some of the secondary values, make a guess.')
-        slider_cut = st.select_slider('Cut quality', options=['Fair', 'Good', 'Very Good', 'Premium', 'Ideal'])
-        slider_color = st.select_slider("Color (letter D means colorless)", options=['Y-Z', 
-                                                                                        'W-X', 
-                                                                                        'W', 
-                                                                                        'U-V', 
-                                                                                        'S-T', 
-                                                                                        'Q-R', 
-                                                                                        'O-P', 
-                                                                                        'O', 
-                                                                                        'N', 
-                                                                                        'M', 
-                                                                                        'L', 
-                                                                                        'K', 
-                                                                                        'J', 
-                                                                                        'I', 
-                                                                                        'H', 
-                                                                                        'G', 
-                                                                                        'F', 
-                                                                                        'E', 
-                                                                                        'D'
-                                                                                    ]
+        slider_cut = st.select_slider('Cut quality', options=['Fair (F)', 
+                                                              'Good (GD)', 
+                                                              'Very Good (VG)', 
+                                                              'Premium', 
+                                                              'Ideal or Excelent (EX)'
+                                                              ]
                                         )
-        slider_clarity = st.select_slider("Clarity (number of inclusions)", options=['Included', 
-                                                                                        'Slightly included (2)', 
-                                                                                        'Slightly included (1)', 
-                                                                                        'Very slightly included (2)', 
-                                                                                        'Very slightly included (1)', 
-                                                                                        'Very, very slightly included (2)', 
-                                                                                        'Very, very slightly included (1)', 
-                                                                                        'Internally flawless', 
-                                                                                        'Flawless'
-                                                                                    ]
+        slider_color = st.select_slider('Color', options=['Y-Z (light)', 
+                                                            'W-X (light)', 
+                                                            'W (light)', 
+                                                            'U-V (light)', 
+                                                            'S-T (light)', 
+                                                            'Q-R (very light)', 
+                                                            'O-P (very light)', 
+                                                            'O (very light)', 
+                                                            'N (very light)', 
+                                                            'M (faint)', 
+                                                            'L (faint)', 
+                                                            'K (faint)', 
+                                                            'J (near colorless)', 
+                                                            'I (near colorless)', 
+                                                            'H (near colorless)', 
+                                                            'G (near colorless)', 
+                                                            'F (colorless)', 
+                                                            'E (colorless)', 
+                                                            'D (colorless)'
+                                                            ]
+                                        )
+        slider_clarity = st.select_slider('Clarity (prevalence of inclusions)', options=['Included (I1)', 
+                                                                                            'Slightly included 2 (SI2)', 
+                                                                                            'Slightly included 1 (SI1)', 
+                                                                                            'Very slightly included 2 (VS2)', 
+                                                                                            'Very slightly included 1 (VS1)', 
+                                                                                            'Very, very slightly included 2 (VVS2)', 
+                                                                                            'Very, very slightly included 1 (VVS1)', 
+                                                                                            'Internally flawless (IF)', 
+                                                                                            'Flawless (FL)'
+                                                                                        ]
                                             )
         submitted = st.form_submit_button('Submit')
         deactivated_button = True
@@ -90,45 +96,45 @@ def predict_from_characteristics():
         else:
             input_lenght = input_diameter / 2
             input_width = input_lenght
-        for key, value in {'Y-Z': -12, 
-                        'W-X': -11, 
-                        'W': -10, 
-                        'U-V': -9, 
-                        'S-T': -8, 
-                        'Q-R': -7, 
-                        'O-P': -6, 
-                        'O': -5, 
-                        'N': -4, 
-                        'M': -3, 
-                        'L': -2, 
-                        'K': -1, 
-                        'J': 0, 
-                        'I': 1, 
-                        'H': 2, 
-                        'G': 3, 
-                        'F': 4, 
-                        'E': 5, 
-                        'D': 6
-                        }.items():
+        for key, value in {'Y-Z (light)': -12, 
+                            'W-X (light)': -11, 
+                            'W (light)': -10, 
+                            'U-V (light)': -9, 
+                            'S-T (light)': -8, 
+                            'Q-R (very light)': -7, 
+                            'O-P (very light)': -6, 
+                            'O (very light)': -5, 
+                            'N (very light)': -4, 
+                            'M (faint)': -3, 
+                            'L (faint)': -2, 
+                            'K (faint)': -1, 
+                            'J (near colorless)': 0, 
+                            'I (near colorless)': 1, 
+                            'H (near colorless)': 2, 
+                            'G (near colorless)': 3, 
+                            'F (colorless)': 4, 
+                            'E (colorless)': 5, 
+                            'D (colorless)': 6
+                            }.items():
             if slider_color == key:
                 slider_color = value
-        for key, value in {'Fair':0, 
-                            'Good': 1, 
-                            'Very Good': 2, 
+        for key, value in {'Fair (F)':0, 
+                            'Good (GD)': 1, 
+                            'Very Good (VG)': 2,
                             'Premium': 3, 
-                            'Ideal': 4
+                            'Ideal or Excelent (EX)': 4
                             }.items():
             if slider_cut == key:
                 slider_cut = value
-        for key, value in {'Included':0, 
-                            'Slightly included (2)': 1, 
-                            'Slightly included (1)': 2, 
-                            'Very slightly included (2)': 3, 
-                            'Very slightly included (1)': 4,
-                            'Very, very slightly included (2)': 5,
-                            'Very, very slightly included (1)': 6,
-                            'Internally flawless': 7,
-                            'Flawless': 8
+        for key, value in {'Included (I1)':0, 
+                            'Slightly included 2 (SI2)': 1, 
+                            'Slightly included 1 (SI1)': 2, 
+                            'Very slightly included 2 (VS2)': 3, 
+                            'Very slightly included 1 (VS1)': 4,
+                            'Very, very slightly included 2 (VVS2)': 5,
+                            'Very, very slightly included 1 (VVS1)': 6,
+                            'Internally flawless (IF)': 7,
+                            'Flawless (FL)': 8
                             }.items():
             if slider_clarity == key:
                 slider_clarity = value      
