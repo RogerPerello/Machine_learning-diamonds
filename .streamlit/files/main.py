f9d0fb1b0d6cef1289f5c105a8bed101544d452c
Roger Perelló <109803180+RogerPerello@users.noreply.github.com>
2023-02-15 18:40:37 +0100

    update

--- .streamlit/files/main.py
+++ .streamlit/files/main.py
@@ -5,9 +5,6 @@ from front_page import *
 from prediction_from_images_page import *
 from prediction_from_characteristics_page import *
 
-with os.scandir() as entries:
-        for entry in entries:
-            print(entry)
 
 # Configuration
 st.set_page_config(page_title='Diamond APPraiser', layout='wide', page_icon='💠')
