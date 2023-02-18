c3eaad979590f77fa56e55f03d256a31f8923fb1
Roger Perelló <109803180+RogerPerello@users.noreply.github.com>
2023-02-18 02:24:14 +0100

    update

--- .streamlit/files/prediction_from_images_page.py
+++ .streamlit/files/prediction_from_images_page.py
@@ -5,4 +5,4 @@ def predict_from_images():
     # Title and subtitle
     st.header('Prediction from images (INCOMPLETE)')
     st.write('Use photos of a diamond to predict its price.')
-    st.write('If you prefer to annotate its characteristics yourself, click on "Prediction from characteristics" from the sidebar menu.')
+    st.write('If you prefer to add its characteristics yourself, click on "Prediction from characteristics" from the sidebar menu.')
