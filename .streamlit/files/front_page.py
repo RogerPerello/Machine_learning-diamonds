a4b32fac5eb9f2a75cb0a6daa89d1a4015d9fbea
Roger Perelló <109803180+RogerPerello@users.noreply.github.com>
2023-02-27 18:21:39 +0100

    update

--- .streamlit/files/front_page.py
+++ .streamlit/files/front_page.py
@@ -28,12 +28,13 @@ Those deposits are prospected and mined. Then, the diamonds are sliced, studied,
     st.write('As a response to that question, the Diamond APPraiser detects the weight and dimensions of a diamond, as well as its clarity, color and cut.')
     st.write('Subsequently, uses those measures to predict an approximate price.')
     st.subheader('How it works')
-    st.write('If you have a photo of a diamond, click on "Prediction from images" from the sidebar menu.')
-    st.write('Otherwise, if you prefer to point out its characteristics yourself, select "Prediction from characteristics".')
+    st.write('If you have a photo of a diamond and its weight, click on "Prediction from images" from the sidebar menu.')
+    st.write('Otherwise, if you prefer to point out all of its characteristics yourself, select "Prediction from characteristics".')
     st.write('Through the dropdown below, you may check some of the inner workings of the app.')
 
     # Additional information
     with st.expander('A peek into the process'):
+        # General information
         st.header('General information')
         st.write('Current inflation is taken into account in real time thanks to [this calculator](https://www.in2013dollars.com/Jewelry/price-inflation).')
         st.write('If the calculator stops working temporarily for some reason, such as website maintenance, inflation is estimated. If that happens, it will be notified upon prediction delivery.')
@@ -43,17 +44,22 @@ Those deposits are prospected and mined. Then, the diamonds are sliced, studied,
         st.write('- Symmetry and polish: which are ignored, because they are dependent on cut quality in general.')
         st.write('- Subjective appreciations: for example, colored diamonds tend to be less valuable. However, a diamond with a fancy color might be valued higher simply because it looks good, or due to current trends.')
         st.write('These factors can usually be neglected when evaluating tiny diamonds. Therefore, the smaller the diamond, the better the prediction.')
+        
+        # Image recognition information
         st.header('Image recognition')
-        st.write('If an image of a diamond is given, the app uses a deep learning model to obtain its characteristics.')
-        st.write('The dataset used to train that first model can be found [here](https://www.kaggle.com/datasets/harshitlakhani/natural-diamonds-prices-images).')
+        st.write('If an image of a diamond is given, the app uses a deep learning model to obtain an approximation its price.')
+        st.write('Then, a second machine learning model uses the weight of the diamond to further tune the target')
+        st.write('The dataset used to train the models can be found [here](https://www.kaggle.com/datasets/harshitlakhani/natural-diamonds-prices-images).')
         st.subheader('Images data sample')
-        st.write('The column "Id" represents each of the images. The rest of the variables are the target.')
-        df_images = pd.read_csv('src/data/processed/images_data_processed.csv')
+        st.write('The column "Id" represents each of the images. "Weight (carat)" is the variable added to the second model. The variable "price" is the target.')
+        df_images = pd.read_csv('src/data/processed/images_data_processed.csv')[['weight (carat)','price']]
         data_sample_images = df_images.sample(100)
         st.write(data_sample_images)
+        
+        # Price prediction through caracteristics information
         st.header('Price prediction')
-        st.write('Once the characteristics of the given diamond are determined, a supervised machine learning model trained with a larger pool of diamonds decides the price.')
-        st.write('The dataset used to train that second model can be found [here](https://www.kaggle.com/datasets/swatikhedekar/price-prediction-of-diamond).')
+        st.write('If the characteristics of a diamond are passed, a supervised machine learning model trained with a large of diamonds decides the price.')
+        st.write('The dataset used to train that last model can be found [here](https://www.kaggle.com/datasets/swatikhedekar/price-prediction-of-diamond).')
         st.subheader('Prices data sample')
         st.write('The column "price" is the target.')
         df_prices = pd.read_csv('src/data/processed/original_processed.csv')
--- .streamlit/files/prediction_from_characteristics_page.py
+++ .streamlit/files/prediction_from_characteristics_page.py
@@ -14,7 +14,7 @@ def predict_from_characteristics():
     # Title and subtitle
     st.header('Prediction from characteristics')
     st.write('Use the attributes of a diamond to predict its price.')
-    st.write('If you prefer to simply upload a photo, click on "Prediction from image" from the sidebar menu.')
+    st.write('If you prefer to simply upload a photo and write down the weight, click on "Prediction from image" from the sidebar menu.')
 
     # Form
     with st.form('Diamond characteristics'):
