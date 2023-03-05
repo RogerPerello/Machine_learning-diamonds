39be1fe831e47ed458ca310cb0d2875b6d5cc43e
Roger Perelló <109803180+RogerPerello@users.noreply.github.com>
2023-03-05 17:49:52 +0100

    update

--- .streamlit/files/front_page.py
+++ .streamlit/files/front_page.py
@@ -48,7 +48,7 @@ Those deposits are prospected and mined. Then, the diamonds are sliced, studied,
         # Image recognition information
         st.header('Prediction from Images')
         st.write('If an image of a diamond is provided, the app uses a MobilenetV3Large transfer learning model to approximate its price.')
-        st.write('Afterwards, a support vector machine regression algorithm (rbf) uses the weight of the diamond to refine the final result.')
+        st.write('Afterwards, a support vector machine regression algorithm (linear) uses the weight of the diamond to refine the final result.')
         st.write('The dataset used to train the models can be found [here](https://www.kaggle.com/datasets/harshitlakhani/natural-diamonds-prices-images).')
         st.subheader('Image Data Sample')
         st.write('Here is a sample of the diamond data. The column "id" represents each of the images. "weight" is the variable added to the second model. The variable "price" is the target.')
